25d1a5c7daf9de4fcf342e6c3f9925339cbe5da7
tx486 <2980499757@qq.com>
2024-10-10 19:25:38 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ll,int> pli;
+typedef tuple<int,int,int> t3i;
+typedef tuple<int,int,int,int> t4i;
+
+constexpr int N = 2e5 + 5;
+constexpr int K = 45;
+constexpr int MOD = 1e9 + 7;
+constexpr int SML = 1e-10;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+
+int n;
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen(R"(D:\c++\Code\vscodeDemo\TEST\in.in)", "r", stdin);
+    freopen(R"(D:\c++\Code\vscodeDemo\TEST\out.out)", "w", stdout);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

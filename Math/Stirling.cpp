4d5b9977a40b5ecebdc254b3c93f032091e9e4ca
tx486 <2980499757@qq.com>
2024-10-13 15:42:52 +0800

    Update Stirling.cpp

--- Math/Stirling.cpp
+++ Math/Stirling.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 static const int MOD=1e9+7;
-vector<vector<long long>> Stirling(int n,int m){//将N个不同的物品划分为M个非空集合的方案数
+vector<vector<long long>> Stirling(int n,int m){//将N个有标号的物品划分为M个无标号非空集合的方案数
     vector<vector<long long>> S(n+1,vector<long long>(m+1));
     S[0][0]=1;
     for(int i=1;i<=n;i++){

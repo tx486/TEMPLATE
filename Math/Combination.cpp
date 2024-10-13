e65fb0a10aba70d6545080f2d16f183a42bbaaf9
tx486 <2980499757@qq.com>
2024-10-13 09:12:22 +0800

    Update Combination.cpp

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Combination{
+
+
+
 using ll=long long;
-private:
-    static constexpr int MOD=1e9+7;
-    static ll fp(ll a,ll b){
+static constexpr int MOD=1e9+7;
+
+vector<ll> fac;
+ll fp(ll a,ll b){
         ll res=1;
         while(b){
             if(b&1) res=res*a%MOD;
@@ -13,19 +16,17 @@ private:
         }
         return res;
     };
-    static ll inv(ll a){
-        return fp(a,MOD-2);
+ll inv(ll a){
+    return fp(a,MOD-2);
+}
+ll comb(int a,int b){
+    return fac[a]*inv(fac[b])%MOD*inv(fac[a-b])%MOD;
+}
+
+void build(int n){
+    fac.resize(n+1);
+    fac[0]=1;
+    for(int i=1;i<=n;i++){
+        fac[i]=fac[i-1]*i%MOD;
     }
-    vector<ll> fac;
-public:
-    Combination(int n){
-        fac.resize(n+1);
-        fac[0]=1;
-        for(int i=1;i<=n;i++){
-            fac[i]=fac[i-1]*i%MOD;
-        }
-    }
-    ll comb(int a,int b){
-        return fac[a]*inv(fac[b])%MOD*inv(fac[a-b])%MOD;
-    }
-};
+}

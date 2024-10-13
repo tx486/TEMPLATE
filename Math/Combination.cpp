869c61cc7db8f4951a60bdbed3f7fe48896dfe52
tx486 <2980499757@qq.com>
2024-10-13 09:39:40 +0800

    Update Combination.cpp

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 using ll=long long;
 static constexpr int MOD=1e9+7;
+
 vector<ll> fac;
 ll fp(ll a,ll b){
-        ll res=1;
-        while(b){
-            if(b&1) res=res*a%MOD;
-            b>>=1;
-            a=a*a%MOD;
-        }
-        return res;
+    ll res=1;
+    while(b){
+        if(b&1) res=res*a%MOD;
+        b>>=1;
+        a=a*a%MOD;
+    }
+    return res;
 }
 ll inv(ll a){
     return fp(a,MOD-2);
